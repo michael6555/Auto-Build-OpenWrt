6e95afad2d6a2c1751d05fb0e722fa825ed603f7
michael6555 <59444010+michael6555@users.noreply.github.com>
2021-06-30 14:06:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,15 +19,16 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git btmod  https://github.com/sirpdboy/luci-theme-btmod' feeds.conf.default
+sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
 
-#git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
-#git clone https://github.com/garypang13/smartdns-le package/smartdns-le
-#wget -P package/lua-maxminddb https://raw.githubusercontent.com/garypang13/openwrt-packages/master/lua-maxminddb/Makefile
-#wget -P package/redsocks2 https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/redsocks2/Makefile
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
-sed -i '$a src-git sirpdboypackage https://github.com/siropboy/mypackages' feeds.conf.default
-sed -i '$a src-git luci-app-bypass https://github.com/garypang13/luci-app-bypass' feeds.conf.default
+git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+git clone https://github.com/garypang13/smartdns-le package/smartdns-le
+wget -P package/lua-maxminddb https://raw.githubusercontent.com/garypang13/openwrt-packages/master/lua-maxminddb/Makefile
+wget -P package/redsocks2 https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/redsocks2/Makefile
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+
+#sed -i '$a src-git luci-app-bypass https://github.com/garypang13/luci-app-bypass' feeds.conf.default
 # Define My Package
 #git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
