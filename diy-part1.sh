86a198c61ca0361799e8f62b396c22b3ce7cede3
michael6555 <59444010+michael6555@users.noreply.github.com>
2021-06-30 14:21:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git btmod  https://github.com/sirpdboy/luci-theme-btmod' feeds.conf.default
+sed -i '$a src-git luci-theme-btmod https://github.com/sirpdboy/luci-theme-btmod' feeds.conf.default
 sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
 
 git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
