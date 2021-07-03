d86efa886a3dcf31ddb86a2a13d76656a9c5bef3
michael6555 <59444010+michael6555@users.noreply.github.com>
2021-07-03 16:51:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@
 # sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 # 修改openwrt登陆地址,把下面的192.168.6.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-1.0修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Xiaomi Redmi Router AC2100'' package/lean/default-settings/files/zzz-default-settings
